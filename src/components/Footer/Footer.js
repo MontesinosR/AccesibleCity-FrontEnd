38508bcf6816f8806618cb412662d780128f3a48
Carlos Montesinos <carlosmontesinos1990@gmail.com>
2022-07-12 14:58:19 +0100

    footer-corrected

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,6 @@ export const Footer = () => {
       A Full-Stack App Created by{" "}
       <a href="https://github.com/MontesinosR" target="_blank" rel="noreferrer">
         MontesinosR
-      </a>{" "}
-      and{" "}
-      <a href="https://github.com/andriusmw/" target="_blank" rel="noreferrer">
-        andriusmw
       </a>
     </footer>
   );
